e586a1d021803b0c23e11630a7d4d80ec6227a99
Varun Banka <88031057+VarunBanka@users.noreply.github.com>
2022-07-20 00:00:04 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,28 +1,22 @@
 import tkinter as tk
 import tkinter.messagebox
 from tkinter.constants import SUNKEN
-
 window = tk.Tk()
 window.title('Claculator-GeeksForGeeks')
 frame = tk.Frame(master=window, bg="skyblue", padx=10)
 frame.pack()
 entry = tk.Entry(master=frame, relief=SUNKEN, borderwidth=3, width=30)
 entry.grid(row=0, column=0, columnspan=3, ipady=2, pady=2)
-
-
 def myclick(number):
 	entry.insert(tk.END, number)
-
-
 def equal():
 	try:
 		y = str(eval(entry.get()))
 		entry.delete(0, tk.END)
 		entry.insert(0, y)
 	except:
+		#code by Varun BAnka
 		tkinter.messagebox.showinfo("Error", "Syntax Error")
-
-
 def clear():
 	entry.delete(0, tk.END)
 
